3d877a96c9e2ea6cf84493f1fc40b35d1162e4ec
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-07-04 13:33:42 +0200

    Added validation rule to sanity cat document

--- _studio/schemas/documents/cat.js
+++ _studio/schemas/documents/cat.js
@@ -6,7 +6,8 @@ export default {
 		{
 			title: 'Name',
 			name:	'name',
-			type: 'string'
+			type: 'string',
+			validation: rule => rule.required()
 		},
 		{
 			title: 'slug',
@@ -14,12 +15,14 @@ export default {
 			type: 'slug',
 			options: {
 				source: 'name'
-			}
+			},
+			validation: rule => rule.required()
 		},
 		{
 			title: 'Breed',
 			name: 'breed',
-			type: 'string'
+			type: 'string',
+			validation: rule => rule.required()
 		},
 		{
 			title: 'Sex',
@@ -30,12 +33,14 @@ export default {
 					'Male',
 					'Female'
 				]
-			}
+			},
+			validation: rule => rule.required()
 		},
 		{
 			title: 'Age',
 			name: 'age',
-			type: 'number'
+			type: 'number',
+			validation: rule => rule.required()
 		},
 		{
 			title: 'Size',
@@ -47,12 +52,14 @@ export default {
 					'Medium',
 					'Large'
 				]
-			}
+			},
+			validation: rule => rule.required()
 		},
 		{
 			title: 'Date of arrival',
 			name: 'date',
-			type: 'date'
+			type: 'date',
+			validation: rule => rule.required()
 		},
 		{
 			title: 'Living with cats',
@@ -65,7 +72,8 @@ export default {
 					"I may be able to live with other cats",
 					"I prefer to live alone"
 				]
-			}
+			},
+			validation: rule => rule.required()
 		},
 		{
 			title: 'Living with dogs',
@@ -76,7 +84,8 @@ export default {
 					"I may be able to live with dogs",
 					"I can't live with dogs"
 				]
-			}
+			},
+			validation: rule => rule.required()
 		},
 		{
 			title: 'Living with children',
@@ -89,7 +98,8 @@ export default {
 					"I prefer to live with adults only",
 					"I can live with any aged child"
 				]
-			}
+			},
+			validation: rule => rule.required()
 		},
 		{
 			title: 'Ideal home location',
@@ -101,18 +111,20 @@ export default {
 					"I can live in just about any location",
 					"I prefer to live in the countryside"
 				]
-			}
+			},
+			validation: rule => rule.required()
 		},
 		{
-			title: 'Hear me woof',
-			name: 'woof',
+			title: 'Hear me meow',
+			name: 'meow',
 			type: 'file',
 			description: 'MP3 or MP4'
 		},
 		{
 			title: 'Description',
 			name: 'description',
-			type: 'text'
+			type: 'text',
+			validation: rule => rule.required()
 		},
 		{
 			title: 'Portrait',
@@ -131,6 +143,7 @@ export default {
 					title: 'Credits'
 				},
 			],
-		},
+			validation: rule => rule.required()
+		}
 	]
 }
