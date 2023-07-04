c53263b70a0d5f0b6de52624595aebddd0cb9367
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-07-04 11:56:20 +0200

    Added more dog options: living with cats or dogs

--- _studio/schemas/documents/dog.js
+++ _studio/schemas/documents/dog.js
@@ -24,12 +24,46 @@ export default {
 		{
 			title: 'Sex',
 			name: 'sex',
-			type: 'string'
+			type: 'string',
+			options: {
+				list: [
+					'male',
+					'female'
+				]
+			}
+		},
+		{
+			title: 'Age',
+			name: 'age',
+			type: 'number'
 		},
 		{
 			title: 'Date of arrival',
 			name: 'date',
 			type: 'date'
+		},
+		{
+			title: 'Living with dogs',
+			name: 'liveWithDogs',
+			type: 'string',
+			options: {
+				list: [
+					"I prefer to live with other dogs",
+					"I can live with other dogs",
+					"I prefer to live alone"
+				]
+			}
+		},
+		{
+			title: 'Living with cats',
+			name: 'liveWithCats',
+			type: 'string',
+			options: {
+				list: [
+					"I may be able to live with cats",
+					"I can't live with cats"
+				]
+			}
 		},
 		{
 			title: 'Description',
