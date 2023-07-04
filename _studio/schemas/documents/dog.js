3f8b26e93235a6507c0e6ffe662eb5857e02915f
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-07-04 11:28:24 +0200

    Added more dog info to dog.js document

--- _studio/schemas/documents/dog.js
+++ _studio/schemas/documents/dog.js
@@ -7,6 +7,18 @@ export default {
 			title: 'Dog name',
 			name:	'dogName',
 			type: 'string',
+		},
+		{
+			title: 'slug',
+			name: 'slug',
+			options: {
+				source: 'dogName'
+			}
+		},
+		{
+			title: 'Breed',
+			name: 'breed',
+			type: 'string',
 		}
 	]
 }
