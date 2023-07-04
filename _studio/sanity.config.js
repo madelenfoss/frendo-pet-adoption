77e6aaab0fbe01072f25e01555e83fc1658bcc01
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-07-04 14:47:09 +0200

    Imported sanity content file to sanity.config

--- _studio/sanity.config.js
+++ _studio/sanity.config.js
@@ -2,6 +2,7 @@ import { deskTool } from 'sanity/desk';
 import { visionTool } from '@sanity/vision';
 
 import schemas from './schemas/schemas.js';
+import content from './structure/content.js';
 
 export default {
 	title: 'Frendo Pet Adoption',
@@ -9,7 +10,15 @@ export default {
 	projectId: '83o3bs7v',
 	dataset: 'production',
 
-	plugins: [deskTool(), visionTool()],
+	plugins: 
+	[
+		deskTool({
+			title: 'Content',
+			name: 'content',
+			structure: content
+		}), 
+
+		visionTool()],
 
 	schema: {
 		types: schemas,
