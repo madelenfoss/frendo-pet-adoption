da172373d6d2e333f5eb825907b10e05f93ef40d
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-07-05 11:19:56 +0200

    Created menuToggle.js, imported to main

---
+++ _app/js/modules/menuToggle.js
@@ -0,0 +1,27 @@
+export default function menuToggle() {
+	let navigationVisible = true;
+
+	const navigationMenu = document.querySelector('.navbar__menu');
+	const navigationMenuIcon = document.querySelector('.navbar__mobile-menu-icon');
+
+	console.log(navigationMenu);
+
+	navigationMenuIcon.addEventListener('click', handleNavigationMenuIconClick);
+
+	function handleNavigationMenuIconClick(event) {
+		toggleNavigation();
+		renderHTML();
+	}
+
+	function toggleNavigation() {
+		navigationVisible = !navigationVisible;
+	}
+
+	function renderHTML() {
+		if (navigationVisible === false) {
+			navigationMenu.classList.add('navbar__menu--visible');
+		} else {
+			navigationMenu.classList.remove('navbar__menu--visible');
+		}
+	}
+}
