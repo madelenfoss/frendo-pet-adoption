7ba7af3ca98822eea572a376a8325555fa6d6573
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-08-01 12:06:03 +0200

    Added pet age converter js file and function

---
+++ _app/js/modules/petAgeConverter.js
@@ -0,0 +1,24 @@
+export default function petAgeConverter() {
+	
+	/*
+	1 human year =
+	8 cat years
+	7 dog years
+	16 budgie years
+	4 parrot years
+	10 bunny years
+	*/
+
+	const humanYearsInput = document.querySelector('#human');
+
+	/* check if element exists before attempting to add an event listener */
+	if (humanYearsInput) {
+		humanYearsInput.addEventListener('input', () => {
+			document.querySelector('.converter__output-dog').innerHTML = humanYearsInput.value * 7;
+			document.querySelector('.converter__output-cat').innerHTML = humanYearsInput.value * 8;
+			document.querySelector('.converter__output-budgie').innerHTML = humanYearsInput.value * 16;
+			document.querySelector('.converter__output-parrot').innerHTML = humanYearsInput.value * 4;
+			document.querySelector('.converter__output-bunny').innerHTML = humanYearsInput.value * 10;
+		});
+	}
+}
