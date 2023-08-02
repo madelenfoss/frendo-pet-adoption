a95dfbb4c03ab242262196fa78f1e4637e4b15cb
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-08-02 18:35:50 +0200

    Imported renderDogData with fetchedData to main.js

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,8 +1,10 @@
 import menuToggle from "./modules/menuToggle.js";
 import petAgeConverter from "./modules/petAgeConverter.js";
 import fetchAllData from "./modules/fetchAllData.js";
+import renderDogData from "./modules/renderDogData.js";
 
 const fetchedData = await fetchAllData();
 
 menuToggle();
 petAgeConverter();
+renderDogData(fetchedData);
