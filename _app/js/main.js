1d3ed3e19896937841cfdf59f824e9cacc74476d
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-08-02 14:06:36 +0200

    Imported fetchAllData to main.js

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,5 +1,7 @@
 import menuToggle from "./modules/menuToggle.js";
 import petAgeConverter from "./modules/petAgeConverter.js";
+import fetchAllData from "./modules/fetchAllData.js";
 
 menuToggle();
 petAgeConverter();
+fetchAllData();
