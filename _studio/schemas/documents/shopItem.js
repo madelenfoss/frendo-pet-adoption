8596f5570232b1fcd6d927037c4e57af0ddc4e09
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-08-02 13:27:48 +0200

    Added shop images field in shopItem

--- _studio/schemas/documents/shopItem.js
+++ _studio/schemas/documents/shopItem.js
@@ -32,9 +32,38 @@ export default {
 			type: 'number'
 		},
 		{
-			title: 'Image',
-			name: 'Image',
-			type: 'image'
-		}
+			title: 'Shop images',
+			name: 'shopImages',
+			type: 'array',
+			of: [
+				{
+					type: 'object',
+					fields: [
+						{
+							title: 'Image',
+							name: 'image',
+							type: 'image'
+						},
+						{
+							title: 'Description',
+							name: 'description',
+							type: 'text'
+						},
+						{
+							title: 'Alternative image description',
+							name: 'altText',
+							description: 'For accessibility. Keep it short and descriptive. Keep field blank if image is purely decorative',
+							type: 'text'
+						},
+						{
+							title: 'Photographer',
+							name: 'photographer',
+							type: 'string',
+						}
+					]
+				}
+			]
+
+		},
 	],
 }
