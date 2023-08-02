8193f1d8d982e2bc61ada6939c02dd228cf7d62a
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-08-02 14:05:58 +0200

    Added sanity fetch query to fetchAllData.js file

---
+++ _app/js/modules/fetchAllData.js
@@ -0,0 +1,32 @@
+import { sanity } from "../sanity.js";
+
+export default async function fetchAllData() {
+	`*[_type in ["dog", "cat", "bunny", "bird", "other", "slideshow", "shopItem"]] {
+		name,
+		"slug": slug.current,
+		breed,
+		sex,
+		age,
+		size,
+		date,
+		description,
+		liveWithChildren,
+		liveWithCats,
+		liveWithDogs,
+		liveWithBunnies,
+		liveWithBirds,
+		idealHome,
+		"portrait": portrait.asset->url,
+		"alternative": portrait.alternative,
+		"credits": portrait.credits,
+		"dogSound": woof.asset->url ,
+		"catSound": meow.asset->url,
+		"birdSound": chirp.asset->url,
+		"slideshowImages": slideshowImages[].image.asset->url,
+		"slideshowAlternative": slideshowImages[].altText,
+		"slideshowCredits": slideshowImages[].photographer,
+	 }`;
+
+	 const allData = await sanity.fetch(query);
+	 
+}
