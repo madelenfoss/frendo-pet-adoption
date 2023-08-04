7c19d7130b0c7d386284bfe0a4d954a5a3d5f787
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-08-04 03:38:33 +0200

    Fetched and rendered animal info

--- _app/js/modules/renderDogData.js
+++ _app/js/modules/renderDogData.js
@@ -1,3 +1,43 @@
 export default function renderDogData(dogs) {
+	// Variables
+	const allDogsContainer = document.querySelector('.animal__container');
 
+	if (allDogsContainer) {
+		renderHTML();
+	};
+
+	function returnDogDOMElement(dog) {
+		const dogName = dog.name;
+		const dogImageUrl = dog.portrait;
+		const dogImageAltText = dog.alternative;
+
+		const dogItemElement = document.createElement('a');
+		const dogImageElement = document.createElement('img');
+		const dogNameElement = document.createElement('h2');
+
+		dogItemElement.classList.add('animal__container-item');
+		dogImageElement.classList.add('animal__container-item-image');
+		dogNameElement.classList.add('animal__container-item-info');
+
+		dogItemElement.setAttribute('href', `/pet-info/?pet-info=${dog.slug}`);
+		dogImageElement.setAttribute('src', dogImageUrl);
+		dogImageElement.setAttribute('alt', dogImageAltText);
+		dogNameElement.textContent = dogName;
+
+		dogItemElement.append(
+			dogImageElement,
+			dogNameElement
+		);
+
+		return dogItemElement;
+	}
+
+	function renderHTML() {
+		dogs.forEach(dog => {
+			if (allDogsContainer) {
+				const dogCardElement = returnDogDOMElement(dog);
+				allDogsContainer.appendChild(dogCardElement);
+			};
+		});
+	}
 }
