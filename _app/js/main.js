3bedbd481ee76cd8e8f1577d6041e1712e62b867
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-08-04 11:15:09 +0200

    Added new filter function for dogs

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,10 +1,10 @@
 import menuToggle from "./modules/menuToggle.js";
 import petAgeConverter from "./modules/petAgeConverter.js";
-import fetchAllData from "./modules/fetchAllData.js";
+import fetchAllPetData from "./modules/fetchAllPetData.js";
 import renderDogData from "./modules/renderDogData.js";
 
-const fetchedData = await fetchAllData();
+const fetchedPetData = await fetchAllPetData();
 
 menuToggle();
 petAgeConverter();
-renderDogData(fetchedData);
+renderDogData(fetchedPetData);
--- _app/js/modules/renderDogData.js
+++ _app/js/modules/renderDogData.js
@@ -2,15 +2,24 @@ export default function renderDogData(dogs) {
 	// Variables
 	const allDogsContainer = document.querySelector('.animal__container');
 
+	// Function to filter out only dogs from the fetched data array
+	function filterDogs(data) {
+	return data.filter(dog => dog._type === "dog");
+	}
+
+	// Filter the dogs data
+	const dogsData = filterDogs(dogs);
+
+	console.log(dogsData);
+  
 	if (allDogsContainer) {
 		renderHTML();
 	};
 
 	function returnDogDOMElement(dog) {
-		const theDogs  = theDogs.find(theDog => theDog._type === 'dog');
-		const dogName = theDogs.name;
-		const dogImageUrl = theDogs.portrait;
-		const dogImageAltText = theDogs.alternative;
+		const dogName = dog.name;
+		const dogImageUrl = dog.portrait;
+		const dogImageAltText = dog.alternative;
 
 		const dogItemElement = document.createElement('a');
 		const dogImageElement = document.createElement('img');
@@ -20,7 +29,7 @@ export default function renderDogData(dogs) {
 		dogImageElement.classList.add('animal__container-item-image');
 		dogNameElement.classList.add('animal__container-item-info');
 
-		dogItemElement.setAttribute('href', `/pet-info/?pet-info=${theDogs.slug}`);
+		dogItemElement.setAttribute('href', `/pet-info/?pet-info=${dog.slug}`);
 		dogImageElement.setAttribute('src', dogImageUrl);
 		dogImageElement.setAttribute('alt', dogImageAltText);
 		dogNameElement.textContent = dogName;
@@ -34,7 +43,7 @@ export default function renderDogData(dogs) {
 	}
 
 	function renderHTML() {
-		dogs.forEach(dog => {
+		dogsData.forEach(dog => {
 			if (allDogsContainer) {
 				const dogCardElement = returnDogDOMElement(dog);
 				allDogsContainer.appendChild(dogCardElement);
