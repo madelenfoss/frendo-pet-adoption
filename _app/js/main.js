64909f0fec6a00cb8897cc417f0c1d3e645219b4
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-09-06 21:25:40 +0200

    Imported renderCatData to main.js

--- _app/js/main.js
+++ _app/js/main.js
@@ -2,9 +2,11 @@ import menuToggle from "./modules/menuToggle.js";
 import petAgeConverter from "./modules/petAgeConverter.js";
 import fetchAllData from "./modules/fetchAllData.js";
 import renderDogData from "./modules/renderDogData.js";
+import renderCatData from "./modules/renderCatData.js";
 
 const fetchedData = await fetchAllData();
 
 menuToggle();
 petAgeConverter();
 renderDogData(fetchedData);
+renderCatData(fetchedData);
