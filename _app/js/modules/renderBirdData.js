55547f816e5f3dae09036383aa620d70716d14ea
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-09-06 21:31:37 +0200

    Added renderBirdData.js file with birds data

---
+++ _app/js/modules/renderBirdData.js
@@ -0,0 +1,55 @@
+export default function renderbirdData(birds) {
+	// Variables
+	const birdPage = document.querySelector('.bird');
+	const allBirdsContainer = document.querySelector('.animal__container');
+	
+	// Function to filter out only birds from the fetched data array
+	 function filterBirds(birds) {
+	 return birds.filter(bird => bird._type === 'bird');
+		}
+
+	// Filter the birds data
+	const birdsData = filterBirds(birds);
+
+	console.log(birdsData);
+  
+	if (birdPage) {
+		renderHTML();
+	};
+
+	function returnbirdDOMElement(bird) {
+		const birdName = bird.name;
+		const birdImageUrl = bird.portrait;
+		const birdImageAltText = bird.alternative;
+
+		const birdItemElement = document.createElement('a');
+		const birdImageElement = document.createElement('img');
+		const birdNameElement = document.createElement('h2');
+
+		birdItemElement.classList.add('animal__container-item');
+		birdImageElement.classList.add('animal__container-item-image');
+		birdNameElement.classList.add('animal__container-item-info');
+
+		birdItemElement.setAttribute('href', `/pet-info/?pet-info=${bird.slug}`);
+		birdImageElement.setAttribute('src', birdImageUrl);
+		birdImageElement.setAttribute('alt', birdImageAltText);
+		birdNameElement.textContent = birdName;
+
+		birdItemElement.append(
+			birdImageElement,
+			birdNameElement
+		);
+
+		return birdItemElement;
+
+	}
+
+	function renderHTML() {
+		birdsData.forEach(birdData => {
+			if (birdPage) {
+				const birdCardElement = returnbirdDOMElement(birdData);
+				allBirdsContainer.appendChild(birdCardElement);
+			}
+		});
+	}
+}
