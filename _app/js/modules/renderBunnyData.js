d815c34ba1e9f361c94c7abb8e5d872537867281
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-09-07 18:25:22 +0200

    Added renderBunnyData.js, added bunny data

---
+++ _app/js/modules/renderBunnyData.js
@@ -0,0 +1,55 @@
+export default function renderBunnyData(bunnies) {
+	// Variables
+	const bunnyPage = document.querySelector('.bunnies');
+	const allBunniesContainer = document.querySelector('.animal__container');
+	
+	// Function to filter out only bunnies from the fetched data array
+	 function filterBunnies(bunnies) {
+	 return bunnies.filter(bunny => bunny._type === 'bunny');
+		}
+
+	// Filter the bunnies data
+	const bunniesData = filterBunnies(bunnies);
+
+	console.log(bunniesData);
+  
+	if (bunnyPage) {
+		renderHTML();
+	};
+
+	function returnBunnyDOMElement(bunny) {
+		const bunnyName = bunny.name;
+		const bunnyImageUrl = bunny.portrait;
+		const bunnyImageAltText = bunny.alternative;
+
+		const bunnyItemElement = document.createElement('a');
+		const bunnyImageElement = document.createElement('img');
+		const bunnyNameElement = document.createElement('h2');
+
+		bunnyItemElement.classList.add('animal__container-item');
+		bunnyImageElement.classList.add('animal__container-item-image');
+		bunnyNameElement.classList.add('animal__container-item-info');
+
+		bunnyItemElement.setAttribute('href', `/pet-info/?pet-info=${bunny.slug}`);
+		bunnyImageElement.setAttribute('src', bunnyImageUrl);
+		bunnyImageElement.setAttribute('alt', bunnyImageAltText);
+		bunnyNameElement.textContent = bunnyName;
+
+		bunnyItemElement.append(
+			bunnyImageElement,
+			bunnyNameElement
+		);
+
+		return bunnyItemElement;
+
+	}
+
+	function renderHTML() {
+		bunniesData.forEach(bunnyData => {
+			if (bunnyPage) {
+				const bunnyCardElement = returnBunnyDOMElement(bunnyData);
+				allBunniesContainer.appendChild(bunnyCardElement);
+			}
+		});
+	}
+}
