72220097a4f020138eb6d96082ddee0885a6b3d4
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-09-26 23:17:01 +0200

    Added pet slug variables to dogDetails.js

--- _app/js/modules/dogDetails.js
+++ _app/js/modules/dogDetails.js
@@ -1,3 +1,10 @@
 export default function dogDetails(dogs) {
-	const dogDetailsContainer = document.querySelector('.pet-info__container')
+	const dogDetailsContainer = document.querySelector('.pet-info__container');
+
+	const urlParams = new URLSearchParams(window.location.search);
+	const dogSlug = urlParams.get('dog');
+	const catSlug = urlParams.get('cat');
+	const birdSlug = urlParams.get('bird');
+	const bunnySlug = urlParams.get('bunny');
+	const otherSlug = urlParams.get('other');
 }
