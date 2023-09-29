eee6edcfd685b5e596070abba620a9bcc31d4202
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-09-29 16:51:42 +0200

    Created more dog details card elements

--- _app/js/modules/dogDetails.js
+++ _app/js/modules/dogDetails.js
@@ -23,13 +23,37 @@ export default function dogDetails(dogs) {
 		const dogDetailsSoundButton = document.createElement('button');
 		const dogDetailsPetInfoAndDescription = document.createElement('div');
 		const dogDetailsPetInfo = document.createElement('ul');
+
 		const dogDetailsPetInfoBreed = document.createElement('li');
+		const dogDetailsPetInfoBreedElement = document.createElement('b');
+		const dogDetailsPetInfoBreedSpan = document.createElement('span');
+
 		const dogDetailsPetInfoSex = document.createElement('li');
+		const dogDetailsPetInfoSexElement = document.createElement('b');
+		const dogDetailsPetInfoSexSpan = document.createElement('span');
+
 		const dogDetailsPetInfoAge = document.createElement('li');
+		const dogDetailsPetInfoAgeElement = document.createElement('b');
+		const dogDetailsPetInfoAgeSpan = document.createElement('span');
+
 		const dogDetailsPetInfoSize = document.createElement('li');
+		const dogDetailsPetInfoSizeElement = document.createElement('b');
+		const dogDetailsPetInfoSizeSpan = document.createElement('span');
+
 		const dogDetailsPetInfoLiveWithDogs = document.createElement('li');
+		const dogDetailsPetInfoLiveWithDogsElement = document.createElement('b');
+		const dogDetailsPetInfoLiveWithDogsSpan = document.createElement('span');
+
 		const dogDetailsPetInfoLiveWithCats = document.createElement('li');
+		const dogDetailsPetInfoLiveWithCatsElement = document.createElement('b');
+		const dogDetailsPetInfoLiveWithCatsSpan = document.createElement('span');
+
 		const dogDetailsPetInfoLiveWithChildren = document.createElement('li');
+		const dogDetailsPetInfoLiveWithChildrenElement = document.createElement('b');
+		const dogDetailsPetInfoLiveWithChildrenSpan = document.createElement('span');
+
 		const dogDetailsPetInfoIdealHome = document.createElement('li');
+		const dogDetailsPetInfoIdealHomeElement = document.createElement('b');
+		const dogDetailsPetInfoIdealHomeSpan = document.createElement('span');
 	}
 }
