ff538f6cd1b3b4acff00eaee0e8d9fedcb44ee1e
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-10-26 23:40:16 +0200

    Added if statement to render sanity slides

--- _app/js/modules/mainSlideshow.js
+++ _app/js/modules/mainSlideshow.js
@@ -4,4 +4,11 @@ export default function mainSlideshow(slides) {
    const slideshowSlides = document.querySelector('.main__slideshow-slides');
    const controlButtonsContainer  = document.querySelector('.main__slideshow-controls');
    const buttonDotsContainer = document.querySelector('.main__slideshow-dots');
+
+   if (slideshowContainer) {
+      renderSanitySlides(),
+      showFirstSlide()
+   }
+
+   
 }
