7e62fc134e295552799f224b5a355281e095ea09
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-10-29 22:43:41 +0100

    Changed slideshow image query

--- _app/js/modules/fetchAllData.js
+++ _app/js/modules/fetchAllData.js
@@ -24,7 +24,7 @@ export default async function fetchAllData() {
 		"dogSound": woof.asset->url ,
 		"catSound": meow.asset->url,
 		"birdSound": chirp.asset->url,
-		"slideshowImages": slideshowImages[].image.asset->url,
+		"slideshowImage": slideshowImages[].image.asset->url,
 		"slideshowDescription": slideshowImages[].description,
 		"slideshowAlternative": slideshowImages[].altText,
 		"slideshowCredits": slideshowImages[].photographer,
