cfcea05b5e875e70e80d06067784650225a0ee5c
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-12-02 16:42:57 +0100

    Changed button img paths in mainSlideshow.js

--- _app/js/modules/mainSlideshow.js
+++ _app/js/modules/mainSlideshow.js
@@ -53,10 +53,10 @@ export default function mainSlideshow(slides) {
       nextButton.classList.add('main__slideshow-next-button');
 
       // Change arrow svg colors
-      previousButtonImage.setAttribute('src', './_app/assets/icons/previous.svg');
+      previousButtonImage.setAttribute('src', './_app/assets/icons/yellow-previous.svg');
       previousButtonImage.setAttribute('aria-label', 'Click button to go to previous slide');
       // previousButtonImage.classList.add('slideshow__button-previous');
-      nextButtonImage.setAttribute('src', './_app/assets/icons/next.svg');
+      nextButtonImage.setAttribute('src', './_app/assets/icons/yellow-next.svg');
       nextButtonImage.setAttribute('aria-label', 'Click button to go to next slide');
 
       // Double check if path is working
